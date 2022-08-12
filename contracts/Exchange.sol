ae2405f341a9b6ee23e600fc704feec8f34c8916
alexbiz4biz <alexbiz4biz@gmail.com>
2022-08-12 11:20:34 -0400

    8 Create Exchange

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -2,13 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./Token.sol";
 
 contract Exchange {
-	address public feeAccount;
+	address public feeAccount, depositTokenAccountDAI;
 	uint256 public feePercent;
+	Token 
+
 
 	constructor(address _feeAccount, uint256 _feePercent){
 		feeAccount = _feeAccount;
 		feePercent = _feePercent;
+	}
+
+	function depositTokens(address _token, uint256 _amount) public {
+		// Transfer tokens to exchange
+		Token(_token).transferFrom(msg.sender, address(this), _amount)
+		// Update balance
+		// emit an event
 	}
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -11,6 +11,11 @@ describe('Exchange', () => {
 	const feePercent = 10
 	
 	beforeEach(async () => {
+		const Exchange = await ethers.getContractFactory('Exchange')
+		const Token = await ethers.getContractFactory('Token')
+
+		token1 = await Token.deploy('DIGIT-AL GOLD','ALGOLD', 1000000)
+
 		accounts = await ethers.getSigners();
 		deployer = accounts[0]
 		feeAccount = accounts[1]
@@ -24,6 +29,33 @@ describe('Exchange', () => {
 
 		it('tracks the fee account', async () => {
 			expect(await exchange.feeAccount()).to.equal(feeAccount.address)
+		})
+
+		it('tracks the fee percent', async () => {
+			expect(await exchange.feePercent()).to.equal(feePercent)
+		})
+
+	})
+
+	describe('Depositing Tokens', () => {
+		let transaction, result
+		let amount = tokens(10)
+
+		beforeEach(async () => {
+			// Approve token
+			transaction = await exchange.connect(token1).depositToken(token1.address, amount)
+			result = await transaction.wait()
+
+			// Deposit token
+
+		})
+	
+		describe('success', () => {
+			it('tracks the token deposit', async () => {
+				expect(await token1.balanceOf(exchange.address)).to.equal(amount)
+			})
+
+
 		})
 
 		it('tracks the fee percent', async () => {
