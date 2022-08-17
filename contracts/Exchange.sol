2e2ce28902e212499ef47d51c870d2e8b390003a
alexbiz4biz <alexbiz4biz@gmail.com>
2022-08-16 21:24:49 -0400

    11 Make Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,9 +8,33 @@ contract Exchange {
 	address public feeAccount;
 	uint256 public feePercent;
 	mapping(address => mapping(address => uint256)) public tokens;
+	mapping(uint256 => _Order) public orders;
+	uint256 public orderCount;
 
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
 	event Withdraw(address token, address user, uint256 amount, uint256 balance);
+	event Order (
+		// Attributers of an order
+		uint256 id,
+		address user,
+		address tokenGet,
+		uint256 amountGet,
+		address tokenGive,
+		uint256 amountGive,
+		uint256 timestamp
+	);
+
+	// A way to model the order
+	struct _Order {
+		// Attributers of an order
+		uint256 id;
+		address user;
+		address tokenGet;
+		uint256 amountGet;
+		address tokenGive;
+		uint256 amountGive;
+		uint256 timestamp;
+	}
 
 
 	constructor(address _feeAccount, uint256 _feePercent){
@@ -39,8 +63,6 @@ contract Exchange {
 		emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
 	}
 
-
-
 	function balanceOf(address _token, address _user)
 		public
 		view
@@ -48,4 +70,37 @@ contract Exchange {
 	{
 		return tokens[_token][_user];
 	}
+
+	function makeOrder(
+		address _tokenGet, 
+		uint256 _amountGet, 
+		address _tokenGive, 
+		uint256 _amountGive
+	) public {
+		// Prevent orders if tokens aren't on exchange
+        require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+
+		orderCount = orderCount + 1;
+
+		orders[orderCount] = _Order(
+			orderCount, 
+			msg.sender,
+			_tokenGet,
+			_amountGet,
+			_tokenGive,
+			_amountGive,
+			block.timestamp
+		);
+
+		emit Order(
+			orderCount, 
+			msg.sender,
+			_tokenGet,
+			_amountGet,
+			_tokenGive,
+			_amountGive,
+			block.timestamp
+		);		
+	}
+
 }
