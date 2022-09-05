a2108293dafac067008ed7b43a9e6fcad6aa352b
alexbiz4biz <alexbiz4biz@gmail.com>
2022-09-05 09:36:10 -0400

    16 Redux pt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,27 @@
 import { useEffect } from 'react'
-import { ethers } from 'ethers'
-import TOKEN_ABI from '../abis/Token.json'
-import '../App.css';
-import config from '../config.json';
+import { useDispatch } from 'react-redux'
+import config from '../config.json'
+
+import { 
+  loadProvider,
+  loadNetwork,
+  loadAccount,
+  loadToken
+} from '../store/interactions'
+
 
 
 function App() {
 
+  const dispatch = useDispatch()
   const loadBlockchainData = async () => {
-    const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
-    console.log(accounts[0])
+    const account = await loadAccount(dispatch)
 
-    // Connect ethers to blockchain
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const { chainId } = await provider.getNetwork()
-    console.log("chainId: ", chainId)
-    console.log("config: ", config)
-    console.log("config[chainId]: ", config[chainId])
-
+    const provider = loadProvider(dispatch)
+    const chainId = await loadNetwork(provider, dispatch)
 
     // Token Smart Contract
-    const token = await new ethers.Contract( config[chainId].DApp.address , TOKEN_ABI , provider )
-    console.log("DApp address: ", token.address)
-    const symbol = await  token.symbol()
-    console.log("DApp symbol: ", symbol)
+    const token = await loadToken(provider, config[chainId].DApp.address, dispatch)
 
   }
 
