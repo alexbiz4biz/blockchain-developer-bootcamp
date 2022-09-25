4e450ebafe929e7983b8c6ed7313313614a450b9
alexbiz4biz <alexbiz4biz@gmail.com>
2022-09-25 11:27:14 -0400

    23 Make Orders

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -73,10 +73,14 @@ const DEFAULT_EXCHANGE_STATE = {
   loaded: false,
   contract: {},
   transaction: { isSuccessful: false},
-  events: [] 
+  allOrders: { loaded: false, data: [] },
+  events: []
+
 }
 
 export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
+  let index, data
+
   switch(action.type) {
     case 'EXCHANGE_LOADED':
       return {
@@ -126,6 +130,55 @@ export const exchange = (state = DEFAULT_EXCHANGE_STATE, action) => {
         transaction: {
           transactionType: 'Transfer',
           isPending: false,
+          isSuccessful: false,
+          isError: true
+        },
+        transferInProgress: false
+      }
+
+    // ***** ORDERS ***** ORDERS ***** ORDERS ***** ORDERS ***** 
+
+    case 'NEW_ORDER_REQUEST':
+      return {
+        ...state,
+        transaction: {
+          transactionType: 'New Order',
+          isPending: true,
+          isSuccessful: false
+        },
+      }
+
+
+    case 'NEW_ORDER_SUCCESS':
+    // Prevent duplicate orders
+    index = state.allOrders.data.findIndex(order => order.id === action.order.Id)
+
+    if(index === -1){
+      data = [...state.allOrders.data, action.order]
+    } else {
+      data = state.allOrders.data
+    }
+
+      return {
+        ...state,
+        allOrders: {
+          ...state.allOrders,
+          data: [...state.allOrders.data, action.order]
+        },
+        transaction: {
+          transactionType: 'New Order',
+          isPending: false,
+          isSuccessful: true
+        },
+        events: [action.event, ...state.events]
+      }
+
+    case 'NEW_ORDER_FAIL':
+      return {
+        ...state,
+        transaction: {
+          transactionType: 'New Order',
+          isPending: true,
           isSuccessful: false,
           isError: true
         },
